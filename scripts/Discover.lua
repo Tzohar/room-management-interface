72b7253aa649a8c7baa798b4d63f7b616d3bf0aa
Tzohar <73159455+Tzohar@users.noreply.github.com>
2026-01-05 11:40:12 +0200

    Create Discover.lua

---
+++ scripts/Discover.lua
@@ -0,0 +1,19 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local StarterGui = script.Parent.Parent
+local Main = StarterGui.Main	
+local DiscoverFrame = Main.Discover
+
+local RoomsHere = DiscoverFrame.RoomsHere 
+local UIGridLayout = RoomsHere.UIGridLayout
+local RoomTemplate = UIGridLayout.RoomTemplate
+
+local Bar = DiscoverFrame.Bar
+local ListMode = Bar.ListMode
+local Filter = Bar.Filter
+local SquaresMode = Bar.SquaresMode
+
+local function LoadRooms()
+
+end
+
